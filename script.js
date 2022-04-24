20270a0dc52baaac5319a65555d8099693b35b62
Vivek Mishra <100134407+NatwarBHAI@users.noreply.github.com>
2022-04-24 15:49:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-function insert(num)   
-{  
-documentdocument.form1.textview.value = document.form1.textview.value + num;  
-}  
-  
-function equal()  
-{  
-var exp = document.form1.textview.value;  
-if(exp)  
-{  
-document.form1.textview.value = eval(exp)  
-}  
-}  
-function backspace()  
-{  
-var exp = document.form1.textview.value;  
-document.form1.textview.value = exp.substring(0, exp.length - 1);
-}  
+function dis(val)
+{
+ document.getElementById("result").value+=val
+}
+function solve()
+{
+ let x = document.getElementById("result").value
+ let y = eval(x)
+ document.getElementById("result").value = y
+}
+function clr()
+{
+ document.getElementById("result").value = ""
+}
